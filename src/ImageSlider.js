d40718bcf84e7188800d8d7fefab2073d84f629d
Christian Bermeo <czaudiovisual@hotmail.com>
2021-06-26 12:44:41 -0400

    Completed assignment

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,24 @@
 // your ImageSlider code here!
+import React from 'react';
+import { Component } from 'react';
+
+
+export default class ImageSlider extends Component {
+
+    constructor () {
+        super ()
+        this.state = {
+            currentSlideIndex : 0
+        }
+    }
+
+
+    render() {
+        return (
+            // <div>
+            // <ImageSlider/>
+            <h1>I am on slide {this.state.currentSlideIndex}</h1>
+            // </div>
+        )
+    }
+}
